c27f747752edfb8f47e4aa1bc60696fa20e46546
Ian-Mlolwa <ianmlolwa2@gmail.com>
2022-07-25 09:34:06 +0300

    added startup

---
+++ form.php
@@ -0,0 +1,162 @@
+<?php
+
+    $con = mysqli_connect("localhost", "root", "", "Huru");
+
+    if(!$con) {
+        die("ERROR: Could not connect."
+            .mysqli_connect_error());
+    }
+
+    if($_SERVER['REQUEST_METHOD'] == "POST") {
+        //something was posted
+        $name = $_REQUEST['name'];
+        $email = $_REQUEST['email'];
+        $address = $_REQUEST['address'];
+        $phone_number = $_REQUEST['phone_number'];
+        $join_as = $_REQUEST['join_as'];
+        $comment = $_REQUEST['comment'];
+        
+        //save to database
+        $sql = "INSERT INTO Registered_Users Values ('$name','$email','$address','$phone_number','$join_as','$comment')";
+        if($con -> query($sql) == TRUE); {
+            echo '<div class="alert alert-primary" role="alert">
+              This is a primary alert—check it out!
+            </div>';
+
+            header ('location:index.php?Success=<div class="alert alert-primary" role="alert">
+            This is a primary alert—check it out!
+          </div>');
+        }
+    }    
+
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Rems School">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+    <title>Huru Startup Network</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-edu-meeting.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/lightbox.css">
+<!--
+
+TemplateMo 569 Edu Meeting
+
+https://templatemo.com/tm-569-edu-meeting
+
+-->
+  </head>
+
+<body>
+
+  <!-- Sub Header -->
+  <div class="sub-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-sm-8">
+          <div class="left-content">
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-4">
+          <div class="right-icons">
+            <ul>
+              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+      <div class="container">
+          <div class="row">
+              <div class="col-12">
+                  <nav class="main-nav">
+                      <!-- ***** Logo Start ***** -->
+                      <a href="index.php" class="logo">
+                          HURU STARTUP NETWORK
+                      </a>
+                      <!-- ***** Logo End ***** -->
+                      <!-- ***** Menu Start ***** -->
+                      <ul class="nav">
+                          <li class="scroll-to-section"><a onclick="history.back()">Back</a></li>
+                          
+                          <!--
+                          <li class="has-sub">
+                              <a href="javascript:void(0)">Pages</a>
+                              
+                              <ul class="sub-menu">
+                                  <li><a href="#top">Upcoming Meetings</a></li>
+                                  <li><a href="#top">Meeting Details</a></li>
+                              </ul>
+                          
+                          </li>
+                        
+                          <li class="has-sub">
+                            <a href="javascript:void(0)">Bookstore</a>
+                            <ul class="sub-menu">
+                                <li><a href="#top">Buy books</a></li>
+                                <li><a href="#top">Stationary & Appliances</a></li>
+                            </ul>
+                        </li>
+                          <li class="scroll-to-section"><a href="#courses">Courses</a></li> 
+                          <li class="scroll-to-section"><a href="#contact">Contact Us</a></li> 
+
+                      -->
+                      </ul>        
+                      <a class='menu-trigger'>
+                          <span>Menu</span>
+                      </a>
+                      <!-- ***** Menu End ***** -->
+                  </nav>
+              </div>
+          </div>
+      </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+
+  <!-- ***** Main Banner Area Start ***** -->
+  <section class="section main-banner" id="top" data-section="section1">
+      <video autoplay muted loop id="bg-video">
+          <source src="assets/images/course-video.mp4" type="video/mp4" />
+      </video>
+
+      <div class="video-overlay header-text">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="caption">
+              <h6>Hello entrepreneurs!</h6>
+              <h2>Welcome to Huru Startup Network</h2>
+              <p>We are a community that connects youth and women innovators/ founders with co-founders, mentors, inevstors and key industry experts to help them bring their innovative ideas to life..</p>
+              <div class="main-button-red">
+                  <div class="scroll-to-section"><a href="#" onclick="history.back()">Back</a></div>
+              </div>
+          </div>
+              </div>
+            </div>
+          </div>
+      </div>
+  </section>
+  <!-- ***** Main Banner Area End ***** -->
+  </html>
---
+++ index.php
@@ -0,0 +1,448 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Rems School">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+    <title>Huru Startup Network</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-edu-meeting.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/lightbox.css">
+<!--
+
+TemplateMo 569 Edu Meeting
+
+https://templatemo.com/tm-569-edu-meeting
+
+-->
+  </head>
+
+<body>
+
+  <!-- Sub Header -->
+  <div class="sub-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-sm-8">
+          <div class="left-content">
+            <p>This is a waiting list for <em>HURU STARTUP NETWORK</em> community. Please enter your details.</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-4">
+          <div class="right-icons">
+            <ul>
+              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+      <div class="container">
+          <div class="row">
+              <div class="col-12">
+                  <nav class="main-nav">
+                      <!-- ***** Logo Start ***** -->
+                      <a href="index.php" class="logo">
+                          HURU STARTUP NETWORK
+                      </a>
+                      <!-- ***** Logo End ***** -->
+                      <!-- ***** Menu Start ***** -->
+                      <ul class="nav">
+                          <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                          <li class="scroll-to-section"><a href="#apply">Join Now</a></li>
+                          
+                          <!--
+                          <li class="has-sub">
+                              <a href="javascript:void(0)">Pages</a>
+                              
+                              <ul class="sub-menu">
+                                  <li><a href="#top">Upcoming Meetings</a></li>
+                                  <li><a href="#top">Meeting Details</a></li>
+                              </ul>
+                          
+                          </li>
+                        
+                          <li class="has-sub">
+                            <a href="javascript:void(0)">Bookstore</a>
+                            <ul class="sub-menu">
+                                <li><a href="#top">Buy books</a></li>
+                                <li><a href="#top">Stationary & Appliances</a></li>
+                            </ul>
+                        </li>
+                          <li class="scroll-to-section"><a href="#courses">Courses</a></li> 
+                          <li class="scroll-to-section"><a href="#contact">Contact Us</a></li> 
+
+                      -->
+                      </ul>        
+                      <a class='menu-trigger'>
+                          <span>Menu</span>
+                      </a>
+                      <!-- ***** Menu End ***** -->
+                  </nav>
+              </div>
+          </div>
+      </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+
+  <!-- ***** Main Banner Area Start ***** -->
+  <section class="section main-banner" id="top" data-section="section1">
+      <video autoplay muted loop id="bg-video">
+          <source src="assets/images/course-video.mp4" type="video/mp4" />
+      </video>
+
+      <div class="video-overlay header-text">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="caption">
+              <h6>Hello entrepreneurs!</h6>
+              <h2>Welcome to Huru Startup Network</h2>
+              <p>We are a community that connects youth and women innovators/ founders with co-founders, mentors, inevstors and key industry experts to help them bring their innovative ideas to life..</p>
+              <div class="main-button-red">
+                  <div class="scroll-to-section"><a href="#contact">Join the difference-makers today!</a></div>
+              </div>
+          </div>
+              </div>
+            </div>
+          </div>
+      </div>
+  </section>
+  <!-- ***** Main Banner Area End ***** -->
+
+  <section class="services">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="owl-service-item owl-carousel">
+          
+            <div class="item">
+              <!--<div class="icon">
+                <img src="assets/images/service-icon-01.png" alt="">
+              </div> -->
+              <div class="down-content">
+                <h4>Co-founder Connect</h4>
+                <p>Huru Startup Network community connects you to technical/non-technical co-founder to help you bring your ideas to life.</p>
+              </div>
+            </div>
+
+            <div class="item">
+              <!--<div class="icon">
+                <img src="assets/images/service-icon-03.png" alt="">
+              </div> -->
+              <div class="down-content">
+                <h4>Venture Studio</h4>
+                <p>Huru Startup Network offers a platform where startups are birthed from idea stage to enterprise. The platform ensures that all viable ideas get the support it requires from community members.</p>
+              </div>
+            </div>
+            
+            <div class="item">
+              <!--<div class="icon">
+                <img src="assets/images/service-icon-02.png" alt="">
+              </div> -->
+              <div class="down-content">
+                <h4>Access to Investors</h4>
+                <p>Angel investors, VCs, donors form an important part of the Huru Startup Network Community. The platform allows founders to meet potential funders and funders to invest in ideas of their interest.</p>
+              </div>
+            </div>
+            
+            <div class="item">
+              <!--<div class="icon">
+                <img src="assets/images/service-icon-02.png" alt="">
+              </div> -->
+              <div class="down-content">
+                <h4>Events</h4>
+                <p>We orgnnize pitching competitions, entrepreneurship conferences that allows the community members to network.</p>
+              </div>
+            </div>
+            
+            <div class="item">
+              <!--<div class="icon">
+                <img src="assets/images/service-icon-03.png" alt="">
+              </div> -->
+              <div class="down-content">
+                <h4>Founders Retreat</h4>
+                <p>Fireside chats that connects new founders with successful entrepreneurs to talk about business and share their experiences.</p>
+              </div>
+            </div>
+
+            <div class="item">
+              <!--<div class="icon">
+                <img src="assets/images/service-icon-03.png" alt="">
+              </div> -->
+              <div class="down-content">
+                <h4>Mentor Connect</h4>
+                <p>Huru Startup Network has a rich pool of professionals from different fields willing to offer mentorship to beginners in business and in areas such as software engineering, product management,fundraising etc. </p>
+              </div>
+            </div>
+
+            <div class="item">
+              <!--<div class="icon">
+                <img src="assets/images/service-icon-03.png" alt="">
+              </div> -->
+              <div class="down-content">
+                <h4>Enthusiast</h4>
+                <p>You love being part of the conversation in matters startups, entrepreneurship, tech and engaging in various activities in the ecosystem? You are welcome! </p>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+
+ <section class="apply-now" id="apply">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 align-self-center">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="item">
+                <h3>JOIN AS A TECHNICAL CO-FOUNDER</h3>
+                <p>Do you skills and are looking for life-changing ideas to be part of? If so, your search has reached to an  end. Join our amazing community today.</p>
+                <div class="main-button-red">
+                  <div class="scroll-to-section"><a href="#contact">Join Us Now!</a></div>
+              </div>
+              </div>
+            </div>
+            <div class="col-lg-12">
+              <div class="item">
+                <h3>JOIN AS AN INNOVATOR</h3>
+                <p>Do you a great viable idea that you believe will be impactful but do not know how to start? If this describes you, you are what Huru Startup Community is looking for.</p>
+                <div class="main-button-yellow">
+                  <div class="scroll-to-section"><a href="#contact">Join Us Now!</a></div>
+              </div>
+              </div>
+            </div>
+            <div class="col-lg-12">
+              <div class="item">
+                <h3>JOIN AS AN INVESTOR</h3>
+                <p>Are you looking forward to investing in impactful ideas and innovators working on life-changing ideas? If so, we would like to have you in onboard.</p>
+                <div class="main-button-yellow">
+                  <div class="scroll-to-section"><a href="#contact">Join Us Now!</a></div>
+              </div>
+              </div>
+            </div>
+            <div class="col-lg-12">
+              <div class="item">
+                <h3>JOIN AS A MENTOR</h3>
+                <p>Do you want to hold someone else's hand and guide them to reach their highest possible potential in their career, business or general life? Your noble contributuion is highly valued. Be part of our community.</p>
+                <div class="main-button-yellow">
+                  <div class="scroll-to-section"><a href="#contact">Join Us Now!</a></div>
+              </div>
+              </div>
+            </div>
+            <div class="col-lg-12">
+              <div class="item">
+                <h3>JOIN AS A DONOR/PHILANTROPIST</h3>
+                <p>Founders working on social impact solutions require a lot of support to bring their ideas to life. The support can range from office space, grants, introductions to name but a few. We welcome all philanthropists willing to support the vision of these amazing dreamers.</p>
+                <div class="main-button-yellow">
+                  <div class="scroll-to-section"><a href="#contact">Join Us Now!</a></div>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="accordions is-first-expanded">
+            <article class="accordion">
+                <div class="accordion-head">
+                    <span>About Huru Startup Network</span>
+                    <span class="icon">
+                        <i class="icon fa fa-chevron-right"></i>
+                    </span>
+                </div>
+                <div class="accordion-body">
+                    <div class="content">
+                        <p>Huru Startup Network is a platform for dreamers with life transforming ideas. We understand the difficulty of launching a startup for first time entrepreneurs as well as experienced ones. Getting a technical/non-technical co-founder, getting infront of investors, testing your ideas and getting relevant industry connections are among the difficulties many founders face in their new ventures. This is where Huru Startup Network comes in, a community of entreprebeurs who support each other to bring their ideas to life.</p>
+                    </div>
+                </div>
+            </article>
+          
+            <article class="accordion">
+                <div class="accordion-head">
+                    <span>Please tell your network</span>
+                    <span class="icon">
+                        <i class="icon fa fa-chevron-right"></i>
+                    </span>
+                </div>
+                <div class="accordion-body">
+                    <div class="content">
+                        <p>Share the good news with your change-makers!<br>
+                        It is our joy if as many people as possible benefit from this great community of entreprebeurs.</p>
+                    </div>
+                </div>
+            </article>
+            
+        </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+
+  <section class="contact-us" id="contact">
+    <div class="container">
+
+
+      <div class="col-lg-12">
+        <div class="section-heading">
+          <h2>Register</h2>
+        </div>
+      </div>
+
+
+      <div class="row">
+        <div class="col-lg-9 align-self-center">
+          <div class="row">
+            <div class="col-lg-12">
+
+               
+              <form id="contact" action="form.php" method="post">
+                <div class="row">
+
+                    
+               
+                <label for="name">Name</label>
+                  <input type="text" placeholder="Your answer"  name="name" required>
+                <label for="email">Email</label>
+                  <input type="email" placeholder="Your answer" name="email" required>
+                <label for="address">Address</label>
+                  <input type="text" placeholder="Your answer" name="address">
+                <label for="phone">Phone Number</label>
+                  <input type="tel" placeholder="Your answer" name="phone_number">
+                <label for="person">Intrested to join as?</label>
+                  <select name="join_as" id="join_as" required>
+                    <option value="" selected disabled hidden>choose</option>
+                    <option value="software-engineer/developer">Software engineer/Developer</option>
+                    <option value="technical co-founder">Technical Co-founder</option>
+                    <option value="angel-investor">Angel Invester</option>
+                    <option value="vc-firm">Vc firm</option>
+                    <option value="donor/philanthropist">Donor/Philanthropist</option>
+                    <option value="mentor">Mentor</option>
+                    <option value="entusiast">Enthusiast</option>
+                  </select>
+                <label for="comments">What are your expectations in the community/platform?</label>
+                  <input type="text" placeholder="Your answer" name="comment">
+                <div class="col-lg-4">
+                  <button type="reset" name="reset" class="button">Clear form</button>
+                </div>
+                <div class="col-lg-4">
+                  <button class="submit" type="submit">Submit</button>
+                </div>
+
+                </div>
+              </form>
+            
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="right-info">
+            <ul>
+              <li>
+                <h6>Phone Number</h6>
+                <span>+254797067476</span>
+                <span>+254708555749</span>
+              </li>
+              
+              <!--<li>
+                <h6>Website URL</h6>
+                <span>www.hurustartupnetwork.netlify.com</span>
+              </li>-->
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+      <p>Copyright © 2022 Huru Startup Network. All Rights Reserved. 
+          
+    </div>
+  </section>
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="assets/js/isotope.min.js"></script>
+    <script src="assets/js/owl-carousel.js"></script>
+    <script src="assets/js/lightbox.js"></script>
+    <script src="assets/js/tabs.js"></script>
+    <script src="assets/js/video.js"></script>
+    <script src="assets/js/slick-slider.js"></script>
+    <script src="assets/js/custom.js"></script>
+    <script>
+        //according to loftblog tut
+        $('.nav li:first').addClass('active');
+
+        var showSection = function showSection(section, isAnimate) {
+          var
+          direction = section.replace(/#/, ''),
+          reqSection = $('.section').filter('[data-section="' + direction + '"]'),
+          reqSectionPos = reqSection.offset().top - 0;
+
+          if (isAnimate) {
+            $('body, php').animate({
+              scrollTop: reqSectionPos },
+            800);
+          } else {
+            $('body, php').scrollTop(reqSectionPos);
+          }
+
+        };
+
+        var checkSection = function checkSection() {
+          $('.section').each(function () {
+            var
+            $this = $(this),
+            topEdge = $this.offset().top - 80,
+            bottomEdge = topEdge + $this.height(),
+            wScroll = $(window).scrollTop();
+            if (topEdge < wScroll && bottomEdge > wScroll) {
+              var
+              currentId = $this.data('section'),
+              reqLink = $('a').filter('[href*=\\#' + currentId + ']');
+              reqLink.closest('li').addClass('active').
+              siblings().removeClass('active');
+            }
+          });
+        };
+
+        $('.main-menu, .responsive-menu, .scroll-to-section').on('click', 'a', function (e) {
+          e.preventDefault();
+          showSection($(this).attr('href'), true);
+        });
+
+        $(window).scroll(function () {
+          checkSection();
+        });
+    </script>
+</body>
+
+</body>
+</html>
